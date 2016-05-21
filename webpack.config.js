082a3277e74829b5132a2793f958e86fb3634847
Akira Osada <osd.akira@gmail.com>
2016-05-21 16:44:21 +0900

    webpack.config sample

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './source/javascripts/app.js',
+    output: {
+        path: './build/',
+        filename: 'app.bundle.js',
+    }
+}
